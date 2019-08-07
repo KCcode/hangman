0be4daaee7f9c1029da3f44740620124ff72a394
Kattia <angorakat@gmail.com>
2019-08-07 13:42:22 -0700

    updates

--- hangman.c
+++ hangman.c
@@ -13,6 +13,10 @@ int randWord();
 void getWord(char listWords[6][16], int word, char *ptrSolution);
 void print(char *ptrSolution);
 int isInWord(char userGuess, char *ptrSolution);
+void initBoard(char *ptrSolution, int *boardSize, char *currentBoard);
+void updateBoard(char *ptrSolution, char *userGuess, char *currentBoard);
+void printBoard(char *currentBoard, int boardSize);
+int winGame(char *ptrSolution, char *currentBoard);
 
 int main(void){
    
@@ -23,24 +27,35 @@ int main(void){
    int tries;
    tries = 10;
    char userGuess;
-//   userGuess = '0';
-   //randomly chose a word from the list to set as the answer
-   //get a char from the user
-   //while there are still tries compare each space with the letter guessed to the answer
+   char currentBoard[10];
+   int boardSize;
+   boardSize = 0;
+   
    getWord(guessWords, randWord(), ptrSolution);   
    print(ptrSolution);
-   while((tries > 0) && ((userGuess = getchar()) != '\n')){
-      //userGuess = getchar();
-      fflush(stdin);
-      if(isInWord(userGuess, ptrSolution) > 0){
-         printf("%s", "yes");
-      }
+   initBoard(ptrSolution, &boardSize, currentBoard);
 
-      else{
-         printf("%s", "no");
-      }
+   while( !(winGame(ptrSolution, currentBoard) == 0) || (tries > 0) ){ // or won game in less or equal tries to tries
       
-      //printf("%s", (isInWord(userGuess, ptrSolution) > 0) ? "true" : "false");
+      printf("%s", "Guess a letter: ");
+      userGuess = getchar();
+      getchar();
+      printf("%c", userGuess);
+           
+      if(isInWord(userGuess, ptrSolution) == 0){
+         tries--;
+         printBoard(currentBoard, boardSize);
+         //printf("%s, %c", "printing user guess in if: ", userGuess);
+      }
+      else{
+      //update board
+      //printf("%c",userGuess);
+      updateBoard(ptrSolution, &userGuess, currentBoard);
+      printBoard(currentBoard, boardSize);
+      
+      //check if won game
+      
+      }
    }   
    return 0;
 }
@@ -64,16 +79,14 @@ void print(char *ptrSolution){
    for(i = 0; i < 10; i++){
       printf("%c", ptrSolution[i]);
    }
-   printf("\n");  
+   printf("%c",'\n');  
 }
 
 //get the user's char and compare to each letter in the solution
 //if 0 not it word if 1 then it is in the word
 int isInWord(char userGuess, char *ptrSolution){
    int i, count;
-   //char userChar;
    count = 0;
-   //userChar = getchar();
    for(i = 0; i < 10; i++){
       if(userGuess == ptrSolution[i]){
          count++;
@@ -82,6 +95,50 @@ int isInWord(char userGuess, char *ptrSolution){
    return count;
 }
 
+void initBoard(char *ptrSolution, int *boardSize, char *currentBoard){
+   while(*(ptrSolution++) != '\0'){
+      printf("%s", "_ ");
+      *(currentBoard++) = '_';
+      (*boardSize)++;
+   }
+   printf("%c",'\n');
+}
+
+void updateBoard(char *ptrSolution, char *userGuess, char *currentBoard){
+   int i;
+   for(i = 0; i < 10; i++){
+      if(ptrSolution[i] == *userGuess){
+         *currentBoard  = *userGuess;
+      }
+      currentBoard++;
+   }
+}
+
+void printBoard(char *currentBoard, int boardSize){
+   int i;
+   for(i = 0; i < boardSize; i++){
+      printf("%c ", currentBoard[i]);
+   }
+}
 
 
-
+int winGame(char *ptrSolution, char *currentBoard){
+   int solutionSize;
+   int counter;
+   solutionSize = 0;
+   counter = 0;
+   while(*(ptrSolution++) != '\n'){
+      solutionSize++;
+//      currentBoard++;
+      if(*ptrSolution == *currentBoard){
+         counter++;
+      }
+      currentBoard++;
+   }
+   if(solutionSize == counter){
+      return solutionSize;
+   }
+   else{
+      return 0;
+   }
+}
